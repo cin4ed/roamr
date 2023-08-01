893a55c2948cfc628f37e953d3aef69534745b55
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2023-07-31 20:40:40 -0700

    x

--- resources/views/layouts/dev.blade.php
+++ resources/views/layouts/dev.blade.php
@@ -13,6 +13,9 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <!-- Extra -->
+        {{ $head }}
     </head>
     <body class="font-sans antialiased">
         {{ $slot }}
