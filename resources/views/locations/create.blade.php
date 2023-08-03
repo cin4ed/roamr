4de2c3e6cac64241752cd2557f8b617c5d8d5ef6
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2023-08-03 14:07:29 -0700

    x

--- resources/views/locations/create.blade.php
+++ resources/views/locations/create.blade.php
@@ -66,13 +66,24 @@
                     <label for="name">Description</label>
                     <input type="text" name="name" placeholder="Some description..." class="mt-1 block w-full h-10 rounded-md bg-neutral-800 border-none placeholder:text-neutral-400">
                 </div>
-                <div class="mt-2">
+                {{-- <div class="mt-2">
                     <label for="description">Tags</label>
                     <input type="text" name="description" placeholder="Some tags..." class="mt-1 block w-full h-10 rounded-md bg-neutral-800 border-none placeholder:text-neutral-400">
-                </div>
+                </div> --}}
                 <div class="mt-2">
-                    <label for="image">Image</label>
-                    <input type="file" name="image">
+                    <p>Image</p>
+                    <div class="flex items-center justify-center w-full mt-2">
+                        <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-48 border-2 border-neutral-600 border-dashed rounded-lg cursor-pointer bg-neutral-800 hover:bg-neutral-700">
+                            <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                                <svg class="w-8 h-8 mb-4 text-neutral-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                                </svg>
+                                <p class="mb-2 text-sm text-neutral-500"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                                <p class="text-xs text-neutral-500">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                            </div>
+                            <input id="dropzone-file" type="file" class="hidden" />
+                        </label>
+                    </div> 
                 </div>
                 <button class="mt-6 bg-neutral-800 px-6 py-2 font-bold rounded-md w-full">Create</button>
             </form>
