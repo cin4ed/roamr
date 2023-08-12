9356f5d9cfa594582ab1396d8bff5768c721c127
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2023-08-11 19:39:39 -0700

    Add Location Migration

---
+++ database/migrations/2023_08_04_210257_create_locations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->text('description');
+            $table->double('longitude', 3, 10);
+            $table->double('latitude', 3, 10);
+            $table->foreignId('user_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('locations');
+    }
+};
