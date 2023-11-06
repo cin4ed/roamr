c8b5126782c6937a50037f76e9d1f399041c0cf9
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2023-11-06 00:05:07 -0700

    Add configuration for sail

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,32 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig, loadEnv } from 'vite'
+import laravel from 'laravel-vite-plugin'
 
-export default defineConfig({
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  const config = {
     plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
-});
+      laravel({
+        input: [
+          'resources/css/app.css',
+          'resources/js/app.js',
+          'resources/js/main.js'
+        ],
+        refresh: true
+      })
+    ]
+  }
+
+  if (env.USING_SAIL) {
+    config.server = {
+      https: false,
+      host: true,
+      port: 3009,
+      hmr: {
+        host: '0.0.0.0', protocol: 'ws'
+      }
+    }
+  }
+
+  return config
+})
