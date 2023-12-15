4c6e6f7c35613d3eecb50f2dce9bf8bf2015e996
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2023-12-15 11:50:45 -0700

    Update welcome view to use new app layout

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,120 +1,89 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<x-app-layout>
+    <x-slot name="head">
+        @include('mapbox-scripts')
+    </x-slot>
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Add new location button -->
+    @if (Auth::check())
+        <x-primary-button-link href="{{ url('/locations/create') }}" id="add-btn" class="text-center">
+            Add +
+        </x-primary-button-link>
+    @endif
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <!-- Map -->
+    <div id="map" class="flex-1 rounded-md border mt-2 border-neutral-700"></div>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Scripts -->
+    <script>
+      // Get lng and lat from url params if they exist
+      const urlParams = new URLSearchParams(window.location.search)
+      const lng = urlParams.get('lng')
+      const lat = urlParams.get('lat')
 
-        <!-- Mapbox -->
-        <script src='https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.js'></script>
-        <link href='https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.css' rel='stylesheet' />
-        <script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.min.js"></script>
-        <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.css" type="text/css">
-    </head>
-    <body class="antialiased dark:bg-neutral-900 dark:text-neutral-400">
-        <div class="p-2 h-screen flex flex-col">
-            <div class="flex justify-between items-center h-10 px-1">
-                <x-primary-link href="{{ url('/') }}">Roamr</x-primary-link>
-                @if (Route::has('login'))
-                    <div>
-                        @auth
-                            <x-primary-link href="{{ url('/profile') }}">Profile</x-primary-link>
-                        @else
-                            <x-primary-link href="{{ route('login') }}">Log in</x-primary-link>
+      // Set default coords
+      const coords = [lng || -99.13401822374149, lat || 19.431459141613338]
 
-                            @if (Route::has('register'))
-                                <x-primary-link href="{{ route('register') }}">Register</x-primary-link>
-                            @endif
-                        @endauth
-                    </div>
-                @endif
-            </div>
-            @if (Auth::check())
-                <x-primary-button-link href="{{ url('/locations/create') }}" id="add-btn" class="text-center">
-                    Add +
-                </x-primary-button-link>
-            @endif
-            <div id="map" class="flex-1 rounded-md  border mt-2 border-neutral-700"></div>
-        </div>
-        <script>
-            // Get lng and lat from url params if they exist
-            const urlParams = new URLSearchParams(window.location.search);
-            const lng = urlParams.get('lng');
-            const lat = urlParams.get('lat');
+      // Initialize mapbox
+      mapboxgl.accessToken =
+        'pk.eyJ1Ijoia2VubmV0aC1xdWludGVybyIsImEiOiJjbGU1bmlsc2gwOHZvM25wM2NwZXVrZnVhIn0.pWzKCuC-uYwHxJm0vThQFQ'
 
-            // Set default coords
-            const coords = [lng || -99.13401822374149, lat || 19.431459141613338];
+      const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/dark-v11',
+        center: coords,
+        zoom: coords ? 13 : 12,
+      })
 
-            // Initialize mapbox
-            mapboxgl.accessToken =
-                "pk.eyJ1Ijoia2VubmV0aC1xdWludGVybyIsImEiOiJjbGU1bmlsc2gwOHZvM25wM2NwZXVrZnVhIn0.pWzKCuC-uYwHxJm0vThQFQ";
+      map.on('load', () => {
+        map.loadImage('https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png', (error, image) => {
+          if (error) throw error
+          map.addImage('custom-marker', image)
 
-            const map = new mapboxgl.Map({
-                container: "map",
-                style: "mapbox://styles/mapbox/dark-v11",
-                center:  coords,
-                zoom: coords ? 13 : 12,
-            });
+          map.addSource('locations', {
+            type: 'geojson',
+            data: "{{ route('api.locations.index') }}",
+          })
 
-            map.on("load", () => {
-                map.loadImage("https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png", (error, image) => {
-                    if (error) throw error;
-                    map.addImage("custom-marker", image);
+          map.addLayer({
+            id: 'locations',
+            type: 'symbol',
+            source: 'locations',
+            layout: {
+              'icon-image': 'custom-marker',
+              'text-field': ['get', 'name'],
+              'text-font': [
+                'Open Sans Semibold',
+                'Arial Unicode MS Bold'
+              ],
+              'text-offset': [0, 1.25],
+              'text-anchor': 'top',
+            },
+            paint: {
+              'text-color': '#ffffff'
+            }
+          })
+        })
+      })
 
-                    map.addSource("locations", {
-                        type: "geojson",
-                        data: "{{ route('api.locations.index') }}",
-                    });
+      // Add geocoder control to the map.
+      // map.addControl(
+      //     new MapboxGeocoder({
+      //         accessToken: mapboxgl.accessToken,
+      //         mapboxgl: mapboxgl
+      //     })
+      // );
 
-                    map.addLayer({
-                        id: "locations",
-                        type: "symbol",
-                        source: "locations",
-                        layout: {
-                            "icon-image": "custom-marker",
-                            "text-field": ["get", "name"],
-                            "text-font": [
-                                "Open Sans Semibold",
-                                "Arial Unicode MS Bold"
-                            ],
-                            "text-offset": [0, 1.25],
-                            "text-anchor": "top",
-                        },
-                        paint: {
-                            "text-color": "#ffffff"
-                        }
-                    });
-                });
-            });
-
-            // Add geocoder control to the map.
-            // map.addControl(
-            //     new MapboxGeocoder({
-            //         accessToken: mapboxgl.accessToken,
-            //         mapboxgl: mapboxgl
-            //     })
-            // );
-
-            // Add geolocate control to the map.
-            // map.addControl(
-            //     new mapboxgl.GeolocateControl({
-            //         positionOptions: {
-            //         enableHighAccuracy: true
-            //         },
-            //         // When active the map will receive updates to the device's location as it changes.
-            //         trackUserLocation: true,
-            //         // Draw an arrow next to the location dot to indicate which direction the device is heading.
-            //         showUserHeading: true
-            //     })
-            // );
-        </script>
-    </body>
-</html>
+      // Add geolocate control to the map.
+      // map.addControl(
+      //     new mapboxgl.GeolocateControl({
+      //         positionOptions: {
+      //         enableHighAccuracy: true
+      //         },
+      //         // When active the map will receive updates to the device's location as it changes.
+      //         trackUserLocation: true,
+      //         // Draw an arrow next to the location dot to indicate which direction the device is heading.
+      //         showUserHeading: true
+      //     })
+      // );
+    </script>
+</x-app-layout>
