8856d94fdfef9d113ee350286049c7493a44aa76
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2023-12-15 11:48:50 -0700

    Create own dropdown

---
+++ resources/views/components/dropdown-simple.blade.php
@@ -0,0 +1,57 @@
+@props(['align' => 'right', 'width' => '48'])
+
+@php
+    switch ($align) {
+        case 'left':
+            $alignmentClasses = 'origin-top-left left-0';
+            break;
+        case 'top':
+            $alignmentClasses = 'origin-top';
+            break;
+        case 'right':
+        default:
+            $alignmentClasses = 'origin-top-right right-0';
+            break;
+    }
+
+    switch ($width) {
+        case '48':
+            $width = 'w-48';
+            break;
+    }
+@endphp
+
+<div class="relative" x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
+    <div @click="open = ! open">
+        <button class="inline-flex border border-transparent text-sm leading-4 rounded-md focus:outline-none transition ease-in-out duration-150
+            text-gray-400 hover:text-gray-500
+            dark:text-neutral-500 dark:hover:text-neutral-400">
+            {{ $trigger_content }}
+            <div class="ml-1">
+                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                     viewBox="0 0 20 20">
+                    <path fill-rule="evenodd"
+                          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                          clip-rule="evenodd"/>
+                </svg>
+            </div>
+        </button>
+    </div>
+
+    <div x-show="open"
+         x-transition:enter="transition ease-out duration-200"
+         x-transition:enter-start="opacity-0 scale-95"
+         x-transition:enter-end="opacity-100 scale-100"
+         x-transition:leave="transition ease-in duration-75"
+         x-transition:leave-start="opacity-100 scale-100"
+         x-transition:leave-end="opacity-0 scale-95"
+         class="absolute z-50 mt-2 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }}"
+         style="display: none;"
+         @click="open = false">
+        <div class="rounded-md shadow-md ring-1 overflow-hidden
+        bg-gray-100 ring-gray-300
+        dark:bg-neutral-800 dark:ring-neutral-700">
+            {{ $content }}
+        </div>
+    </div>
+</div>
