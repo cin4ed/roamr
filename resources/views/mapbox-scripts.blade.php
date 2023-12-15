5abf432ac2764777494ec265bfbc97c43ba6c5c2
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2023-12-15 11:49:42 -0700

    Create mapbox-scripts

---
+++ resources/views/mapbox-scripts.blade.php
@@ -0,0 +1,5 @@
+<!-- Mapbox -->
+<script src='https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.js'></script>
+<link href='https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.css' rel='stylesheet' />
+<script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.min.js"></script>
+<link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.css" type="text/css">
