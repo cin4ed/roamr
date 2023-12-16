aa52d135d928f70777974a5b699cb7f095cc04e2
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2023-12-16 11:58:47 -0700

    Ran pint

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if(env('FORCE_HTTPS',false)) {
+        if (env('FORCE_HTTPS', false)) {
             URL::forceScheme('https');
         }
     }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,10 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\File;
-use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
