575b04870dc7f29b18edd459545b88515fb65530
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2025-02-19 11:03:10 -0700

    wip: testing new map tiles

--- app/page.tsx
+++ app/page.tsx
@@ -68,15 +68,49 @@ export default function Home() {
               latitude: 40,
               zoom: 3.5,
             }}
-            maxZoom={15.9}
+            maxZoom={18}
             style={{ width: "100%", height: "100%" }}
+            mapStyle={{
+              version: 8,
+              sources: {
+                "raster-tiles": {
+                  type: "raster",
+                  tiles: [
+                    // NOTE: Layers from Stadia Maps do not require an API key for localhost development or most production
+                    // web deployments. See https://docs.stadiamaps.com/authentication/ for details.
+                    "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+                  ],
+                  tileSize: 256,
+                  // attribution:
+                  //   'Map tiles by <a target="_blank" href="https://stamen.com">Stamen Design</a>; Hosting by <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a>. Data &copy; <a href="https://www.openstreetmap.org/about" target="_blank">OpenStreetMap</a> contributors',
+                },
+              },
+              layers: [
+                {
+                  id: "simple-tiles",
+                  type: "raster",
+                  source: "raster-tiles",
+                  minzoom: 0,
+                  maxzoom: 20,
+                },
+              ],
+            }}
             // mapStyle="https://tiles.openfreemap.org/styles/bright"
             // mapStyle="https://geoserveis.icgc.cat/contextmaps/icgc_orto_estandard.json"
-            mapStyle="https://raw.githubusercontent.com/go2garret/maps/main/src/assets/json/arcgis_hybrid.json"
+            // mapStyle="https://raw.githubusercontent.com/go2garret/maps/main/src/assets/json/arcgis_hybrid.json"
           >
-            <Source id="my-data" type="geojson" data={geojson}>
+            <Source
+              id="world-imagery"
+              type="raster"
+              tiles={[
+                "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+              ]}
+              tileSize={256}
+            />
+
+            {/* <Source id="my-data" type="geojson" data={geojson}>
               <Layer {...layerStyle} />
-            </Source>
+            </Source> */}
           </Map>
         </div>
       </ResizablePanel>
