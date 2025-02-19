1c945a5333b525a22fe55fc7f8476dd3f50d94ac
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2025-02-19 03:15:19 -0700

    feat: add a search bar for worldwide locations
    Closes #3

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,19 @@
 "use client";
 
-// import { Map, Source, Layer } from "@vis.gl/react-maplibre";
-// import type { CircleLayer } from "@vis.gl/react-maplibre";
 import Map, { Source, Layer } from "react-map-gl/maplibre";
+import type { LngLatLike, MapRef } from "react-map-gl/maplibre";
 import type { CircleLayerSpecification as CircleLayer } from "react-map-gl/maplibre";
-
-// import type { CircleLayer } from "react-map-gl/maplibre";
-
-import type { FeatureCollection } from "geojson";
+import type { FeatureCollection, Point } from "geojson";
 import "maplibre-gl/dist/maplibre-gl.css";
+import { useRef } from "react";
 
 import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
+
+import SearchBar from "@/components/SearchBar";
 
 const geojson: FeatureCollection = {
   type: "FeatureCollection",
@@ -38,26 +37,38 @@ const layerStyle: CircleLayer = {
 };
 
 export default function Home() {
+  const mapRef = useRef<MapRef>(null);
+
   return (
     <ResizablePanelGroup
       direction="horizontal"
       className="min-h-screen font-[family-name:var(--font-geist-sans)]"
     >
-      <ResizablePanel defaultSize={25}>
-        <div className="h-full p-6">
-          <h1>This is roamr</h1>
+      <ResizablePanel defaultSize={30}>
+        <div className="h-full p-6 space-y-5">
+          <div>
+            <SearchBar
+              onResultSelect={(f) => {
+                mapRef.current?.flyTo({
+                  center: (f.geometry as Point).coordinates as LngLatLike,
+                  zoom: 13,
+                });
+              }}
+            />
+          </div>
         </div>
       </ResizablePanel>
       <ResizableHandle withHandle />
-      <ResizablePanel defaultSize={75}>
+      <ResizablePanel defaultSize={70}>
         <div className="h-full">
           <Map
+            ref={mapRef}
             initialViewState={{
               longitude: -100,
               latitude: 40,
               zoom: 3.5,
             }}
-            // maxZoom={15.9}
+            maxZoom={15.9}
             style={{ width: "100%", height: "100%" }}
             // mapStyle="https://tiles.openfreemap.org/styles/bright"
             // mapStyle="https://geoserveis.icgc.cat/contextmaps/icgc_orto_estandard.json"
