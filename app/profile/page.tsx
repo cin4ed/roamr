94bf1f05b9d901cca87f1a5450694d7176f053fb
VictorMRD <victormrd2002@hotmail.com>
2025-02-22 18:42:46 -0700

    Create boilerplate code for profile-page

---
+++ app/profile/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NextPage } from 'next';
+
+const ProfilePage: NextPage = () => {
+    return (
+        <div>
+            <h1>Profile Page</h1>
+            <p>Welcome to the profile page!</p>
+        </div>
+    );
+};
+
+export default ProfilePage;
