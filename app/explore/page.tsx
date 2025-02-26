c51d916a4262a7abfa83da495647b0158e502789
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2025-02-26 13:08:43 -0700

    chore: create urbex-community-invite-card.tsx

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { AppSidebar } from "@/components/app-sidebar";
+import { UrbexCommunityInviteCard } from "@/components/urbex-community-invite-card";
 import { useAuth } from "@/hooks/useAuth";
 import { useEffect } from "react";
 
@@ -11,9 +12,6 @@ import {
 } from "@/components/ui/resizable";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Session } from "@supabase/supabase-js";
-import { Separator } from "@/components/ui/separator";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
 
 export default function Explore() {
   const { session } = useAuth();
@@ -40,39 +38,7 @@ function LeftPanel({ session }: { session: Session | null }) {
   return (
     <ResizablePanel defaultSize={40} className="min-w-[550px]">
       <ScrollArea className="h-screen p-6">
-        {!session && (
-          <div className="space-y-10 mt-5 border p-4 rounded-md bg-zinc-900">
-            <div>
-              <div>
-                <h2 className="text-xl font-bold">
-                  Create an Account to Explore & Contribute!
-                </h2>
-                <p className="text-sm text-zinc-400">
-                  Join the Roamr urbex community and unlock exclusive
-                  features.{" "}
-                </p>
-                <Separator className="mt-2" />
-              </div>
-              <div className="mt-2 space-y-4">
-                <p>With an account, you can:</p>
-                <ul className="list-disc list-inside space-y-1">
-                  <li>📍 Contribute to the map by adding new locations.</li>
-                  <li>⭐️ Rate, review, and leave comments on urbex spots.</li>
-                  <li>
-                    👥 Connect with fellow explorers and plan trips together.
-                  </li>
-                  {/* <li>
-                    💬 Join our exclusive Discord to share experiences, get
-                    tips, and stay updated on new features!
-                  </li> */}
-                </ul>
-                <Button type="button">
-                  <Link href="/login">Create an Account</Link>
-                </Button>
-              </div>
-            </div>
-          </div>
-        )}
+        {!session && <UrbexCommunityInviteCard />}
       </ScrollArea>
     </ResizablePanel>
   );
---
+++ components/urbex-community-invite-card.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+export function UrbexCommunityInviteCard({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"div">) {
+  return (
+    <div
+      className={cn(
+        "space-y-10 border p-4 rounded-md bg-zinc-900 xl:text-sm 2xl:text-base",
+        className,
+      )}
+      {...props}
+    >
+      <div>
+        <div>
+          <h2 className="text-xl font-bold">
+            Create an Account to Explore & Contribute!
+          </h2>
+          <p className="text-sm text-zinc-400">
+            Join the Roamr urbex community and unlock exclusive features.{" "}
+          </p>
+          <Separator className="mt-2" />
+        </div>
+        <div className="mt-2">
+          <p>With an account, you can:</p>
+          <ul className="list-disc list-inside space-y-1 mt-2">
+            <li>📍 Contribute to the map by adding new locations.</li>
+            <li>⭐️ Rate, review, and leave comments on urbex spots.</li>
+            <li>👥 Connect with fellow explorers and plan trips together.</li>
+          </ul>
+          <div className="flex justify-end   mt-5">
+            <Button type="button">
+              <Link href="/login">👉 Sign In | Log In</Link>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
