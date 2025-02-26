4b60dacccc4821234a726e6c3391f33f424e75a7
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2025-02-26 13:15:23 -0700

    chore: update sign-out-button styles

--- components/sign-out-button.tsx
+++ components/sign-out-button.tsx
@@ -4,8 +4,9 @@ import { LogOut } from "lucide-react";
 
 export function SignOutButton() {
   return (
-    <Button type="button" onClick={() => signOut()} className="w-10 h-10">
+    <Button type="button" onClick={() => signOut()}>
       <LogOut className="w-10 h-10" />
+      Log Out
     </Button>
   );
 }
