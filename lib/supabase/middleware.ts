3e9828b2b7c3bb7de808ffa84f368f7ed3514b5d
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2025-02-26 13:20:03 -0700

    feat: protect /profile page from unauthenticated users

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -30,7 +30,11 @@ export async function updateSession(request: NextRequest) {
   );
 
   // refreshing the auth token
-  await supabase.auth.getUser();
+  const user = await supabase.auth.getUser();
+
+  if (request.nextUrl.pathname.startsWith("/profile") && user.error) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
 
   return supabaseResponse;
 }
