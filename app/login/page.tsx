47588953e2494766ec544b9abf3dc3954a775a26
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2025-03-19 13:45:54 -0700

    feat: enhance login page with logo and background texture

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,15 @@
 import { LoginForm } from '@/components/login-form';
+import roamrLogo from '@/public/roamr-logo.png';
+import Image from 'next/image';
 
 export default function Page() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <LoginForm />
+        <div className="flex flex-col items-center gap-7">
+          <Image src={roamrLogo} alt="Roamr Logo" width={175} />
+          <LoginForm className="w-full" />
+        </div>
       </div>
     </div>
   );
